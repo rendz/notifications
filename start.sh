3dea735fb6925f84e836d40464e1fdf48fa9792d
daniel <d.zhelyazkov@mail.mail>
2022-02-15 21:19:12 +0000

    Add Kafka broker docker-compose.yml and scripts

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+CWD="$( pwd -P )"
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd ${SCRIPTPATH}/kafka
+docker-compose up -d
+cd $CWD
+
+until docker exec broker kafka-topics --list --bootstrap-server broker:9092 &> /dev/null
+do
+    echo "Waiting for kafka to start ..."
+    sleep 1
+done
+
+docker exec broker \
+kafka-topics --bootstrap-server broker:9092 \
+             --create \
+             --topic slack-notifications
+
+docker exec broker \
+kafka-topics --bootstrap-server broker:9092 \
+             --create \
+             --topic email-notifications
+
+docker exec broker \
+kafka-topics --bootstrap-server broker:9092 \
+             --create \
+             --topic sms-notifications
