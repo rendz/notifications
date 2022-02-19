16c1bf7be2fe3b3a29169cd38aeee78c9a87e34b
daniel <d.zhelyazkov@mail.mail>
2022-02-19 15:55:02 +0000

    Add request validation

--- notifications-server/src/main/java/io/notifications/server/adapter/rest/NotificationsController.java
+++ notifications-server/src/main/java/io/notifications/server/adapter/rest/NotificationsController.java
@@ -1,17 +1,25 @@
 package io.notifications.server.adapter.rest;
 
 import io.notifications.server.adapter.rest.dto.EmailNotificationDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-@RestController
-@RequestMapping("/api/notifications/email")
-public class NotificationsController {
+import javax.validation.Valid;
 
-    @PostMapping
-    Mono<Void> publishNotification(EmailNotificationDTO notificationDTO) {
+@RestController
+@RequestMapping(value = "/api/notifications/email", consumes = MediaType.APPLICATION_JSON_VALUE)
+public class NotificationsController {
+    Logger logger = LoggerFactory.getLogger(NotificationsController.class);
+
+    @PostMapping(consumes = {"application/json"})
+    Mono<Void> publishNotification(@Valid @RequestBody EmailNotificationDTO notificationDTO) {
+        logger.info(notificationDTO.toString());
         return Mono.empty();
     }
 }
--- notifications-server/src/main/java/io/notifications/server/adapter/rest/dto/EmailNotificationDTO.java
+++ notifications-server/src/main/java/io/notifications/server/adapter/rest/dto/EmailNotificationDTO.java
@@ -2,9 +2,14 @@ package io.notifications.server.adapter.rest.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class EmailNotificationDTO {
+    @NotNull
     private String sender;
+    @NotNull
     private String receiver;
+    @NotNull
     private String message;
 }
